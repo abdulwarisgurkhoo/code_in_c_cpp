57aa7e7fd90d6f0fb08c5c63838117672fefebe9
abdul waris gurkhoo <gurkhooabdulwaris@gmail.com>
2024-08-30 15:23:03 +0530

    c and cpp program

---
+++ .vscode/sum_of_two_number.cpp
@@ -0,0 +1,16 @@
+/**********************to print the sum of two numbers by taking input from the user************/
+#include<iostream>
+using namespace std;
+int main ()
+    {
+        float num1,num2;    // deleration of 2 real number type variables
+        cout<<"please enter two number whose addition you want to print \n";
+        cin>>num1;   // taking input from user
+        cin>>num2;   // taking inp[ut from user 
+        cout<<"the addition of two user enterd number is : "<<num1+num2;
+        return 0;
+
+    }
+
+
+
---
+++ my_first_program.c
@@ -0,0 +1,8 @@
+/**********************to print hello world**********************/
+#include<stdio.h>
+int main ()
+    {
+        printf ("HELLO WORLD \n");  // std in and output function use to print 
+        return 0;
+
+    }
